5bcd2797bdbf669e3bed8660d3866d1fc13ba431
PythonicNerd <neilaylor@hotmail.com>
2017-12-04 16:48:35 -0600

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,11 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.assets.initialize_on_precompile = false
+  config.serve_static_files = true
+  config.assets.compile = true
+
+  
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
@@ -84,7 +89,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-config.assets.initialize_on_precompile = false
-config.serve_static_files = true
-config.assets.compile = true
